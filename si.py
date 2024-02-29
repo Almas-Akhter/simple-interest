0bd4f6baccf48943ecb26a735f56353f37a4ecd8
Almas-Akhter <160632064+Almas-Akhter@users.noreply.github.com>
2024-02-29 17:39:15 +0530

    Create si.py

---
+++ si.py
@@ -0,0 +1,5 @@
+principal = float(input("Enter Principal Amount: "))
+rate=float(input ("Enter the Rate: "))
+time_period=int(input('Enter Time Period: '))
+simpleInterest=(principal*rate*time_period)/100
+print ('Your Simple Intrest is ', simpleInterest, 'rs')
